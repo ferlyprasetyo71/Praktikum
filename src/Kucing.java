89b19b0f8524a455d5e66b1f8e43443292723135
Ferly Yanuar Prasetyo <ferlyprasetyo0@gmail.com>
2022-11-18 09:42:58 +0700

    commit Ferly yanuar prasetyo

---
+++ src/Kucing.java
@@ -0,0 +1,6 @@
+public class Kucing extends Hewan{
+   public void bergerak() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+       System.out.println("Berjalan dengan Kaki, \"Tap..Tap..\"");
+   }
+}
